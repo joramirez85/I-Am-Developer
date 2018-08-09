e729a181ab2b38908a447652c1607aea079f0c5b
Jesus Ramirez <jesus.ramirez@GDLA-MC-180723B.local>
2018-08-08 23:33:20 -0500

    Import Project

---
+++ I Am Developer/I Am Developer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Developer
+//
+//  Created by Jesus Ramirez on 8/8/18.
+//  Copyright © 2018 Jesus Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
